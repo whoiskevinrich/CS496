3b724b819166e1e04fbd880bf059be876b5c0e7d
whoiskevinrich <whoiskevinrich@gmail.com>
2015-04-01 15:47:38 -0700

    Added files to repo

---
+++ HW1-Universities/App_Start/FilterConfig.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Universities
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ HW1-Universities/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+using Universities;
+
+[assembly: OwinStartup(typeof(Startup))]
+namespace Universities
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
